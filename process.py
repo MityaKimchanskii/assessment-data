2fd2ca8a322da0f329a179544e0b4160c17a8139
Dmitrii Kim <61066582+MityaKimchanskii@users.noreply.github.com>
2021-12-10 13:39:54 -0800

    Final commit

--- process.py
+++ process.py
@@ -1,12 +1,21 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # this means we create new variable "log_file" which is text file with data, and for work we open this file
 
 
-def sales_reports(log_file):
+def sales_reports(log_file):  # It's a function called "sales_reports" that has 1 parameter "log_file"
+    for line in log_file:     # we looping over this text file each line 
+        line = line.rstrip()  # then we use method "rstrip()" for each line
+        day = line[0:3]       # then we create new variable "day" equals to line[0:3] which means three first letters
+        if day == "Mon":      # and then we checked if it's equals "Mon" or any day of week 
+            print(line)       # if it's true then print line if not false and skip the line
+
+
+# sales_reports(log_file)       # this line means we are call the function to see and check result
+
+def melons(log_file):
     for line in log_file:
         line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
+        order = int(line[16:18])
+        if order > 10:
             print(line)
 
-
-sales_reports(log_file)
+melons(log_file)
